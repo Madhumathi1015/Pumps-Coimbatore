bc2916812f0ed76593db783df230b15e95fa8275
Madhumathi1015 <131219026+Madhumathi1015@users.noreply.github.com>
2023-04-19 07:21:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ export const App: React.FC<React.PropsWithChildren<AppProps>> = ({
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{Welcome to the world of Pumps}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Create your own AI chat bot" />
+        <meta name="description" content="Your own pump bot!" />
       </Head>
       {children}
     </>
