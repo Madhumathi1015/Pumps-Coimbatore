ac0f8b32b1e13ce20de7302c378242e37b06abfd
Madhumathi1015 <131219026+Madhumathi1015@users.noreply.github.com>
2023-04-19 08:02:46 +0530

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,11 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hai I am your personalised pump bot</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      Warm Greetings from Pumps Coimbatore! We are happy to help you for all your fluid delivery needs. Here is our pump bot to help you.
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
+      
     </p>
   </div>
 );
